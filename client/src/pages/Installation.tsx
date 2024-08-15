fe6f89d8add4cca8ac476e5737cf8e4c7a14242e
Aser <ajhubero16@gmail.com>
2024-08-15 21:10:28 +0800

    Completed the installation guide

--- client/src/pages/Installation.tsx
+++ client/src/pages/Installation.tsx
@@ -1,14 +1,14 @@
 import React from "react";
 import CurrLocation from "../components/CurrLocation";
 import { Link } from "react-router-dom";
-
+import STEP1 from "../assets/imgs/install1.jpg";
+import STEP2 from "../assets/imgs/install2.jpg";
+import STEP3 from "../assets/imgs/install3.jpg";
 function Installation() {
   return (
     <div>
       <CurrLocation location="Installation Guide" />
-
       <h1 className="mb-5 text-mainColor text-6xl">Installation</h1>
-
       <p className="text-textColor text-xl mb-10">
         Ang pag-install or pag-download ng{" "}
         <span className="font-semibold">OsmAnd</span> ay madali lang. Para ka
@@ -39,7 +39,6 @@ function Installation() {
         </span>{" "}
         )
       </p>
-
       <div className="mb-10">
         <h3 className="text-textColor text-2xl font-semibold">
           Step 1: Buksan ang Playstore or AppleStore
@@ -52,8 +51,59 @@ function Installation() {
           ). Hanapin naman ang Apple Store kung Ipad o Iphone ang gamit ninyo.
         </p>
 
-        {/* <img src={NoSpaces} alt="" className="h-[600px]" /> */}
+        <img
+          src={STEP1}
+          alt="Installation Guide. Step 1"
+          className="h-[600px]"
+        />
       </div>
+      <div className="mb-10">
+        <h3 className="text-textColor text-2xl font-semibold">
+          Step 2: Hanapin ang OsmAnd App
+        </h3>
+        <p className="text-textColor text-lg mb-5">
+          Hapanin ang Search Icon at i-type lamang ang OsmAnd. Mayroong icon na
+          kulay orange ang lalabas at i-click lamang iyon. I-click ang install
+          button at hayaang matapos. Kapag natapos na, i-click lamang ang Open
+          button
+        </p>
+
+        <img
+          src={STEP2}
+          alt="Installation Guide. Step 2"
+          className="h-[600px]"
+        />
+      </div>
+
+      <div className="mb-10">
+        <h3 className="text-textColor text-2xl font-semibold">
+          Step 3: I-download ang Maps
+        </h3>
+        <p className="text-textColor text-lg mb-5">
+          Kapag binuksan ang application, ang larawan sa baba ang unang
+          makikita. I-click lamang ang download button at hayaang matapos.{" "}
+        </p>
+
+        <img
+          src={STEP3}
+          alt="Installation Guide. Step 3"
+          className="h-[600px] mb-5"
+        />
+
+        <p className="text-textColor text-lg mb-5">
+          Taaadaaaa, tapos kana sa Step 1, ang pag i-install ng ating
+          application. Ngayon kailangan na nating i-setup ang account na
+          gagamitin din ng buong congregation. Katulad ng nakasanayan, just
+          click the link below!
+        </p>
+      </div>
+
+      <Link
+        to="/setup"
+        className="px-4 py-2 text-lg text-textColor bg-mainColor rounded-2xl hover:bg-mainColorDark"
+      >
+        Click Here: Account Setup
+      </Link>
     </div>
   );
 }
